5af3a8d85a1427448d4b8d63d30a3e3cd4712d4a
Craig <cmcnamara87@gmail.com>
2015-02-03 10:54:00 +1000

    refactor(phenotype-service): add lookup from server

--- app/scripts/services/phenotypeservice.js
+++ app/scripts/services/phenotypeservice.js
@@ -8,7 +8,7 @@
  * Factory in the formsApp.
  */
 angular.module('formsApp')
-    .factory('phenotypeService', function ($q) {
+    .factory('phenotypeService', function ($http, $q) {
         return {
             autocomplete: autocomplete
         };
@@ -19,56 +19,17 @@ angular.module('formsApp')
             return getMatchingPhenotypeTerms(query);
         }
 
-        //function getPhenotypeRefreshUrl(term) {
-        //    var HPO_TERM_URL =
-        // 'http://archive-demo.skeletome.org/drupal/api/hpo.json?callback=JSON_CALLBACK&parameters[name]=';
-        //    return HPO_TERM_URL + term;
-        //}
+        function getAutocompleteUrl(term) {
+            return 'http://archive-demo.skeletome.org/drupal/api/hpo.json?parameters[name]=' + term;
+        }
 
         function getMatchingPhenotypeTerms(term) {
-
             if(!term || !term.length) {
                 return $q.when([]);
             }
 
-            return $q.when(
-                [
-                    {
-                        id: '3624',
-                        name: 'Dwarfism',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0001516'
-                    },
-                    {
-                        id: '3625',
-                        name: 'Dwarfism recognizable at birth',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0008930'
-                    },
-                    {
-                        id: '3626',
-                        name: 'Dwarfism, \'low birth weight\' type',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0001422'
-                    },
-                    {
-                        id: '3627',
-                        name: 'Dwarfism, birth weight normal',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0008907'
-                    },
-                    {
-                        id: '3628',
-                        name: 'Dwarfism, neonatal short-limbed',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0008902'
-                    },
-                    {
-                        id: '3629',
-                        name: 'Dwarfism, short limb mesomelic',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0008910'
-                    },
-                    {
-                        id: '3630',
-                        name: 'Dwarfism, short-trunk, short-limbed',
-                        uri: 'http://purl.obolibrary.org/obo/HP_0008867'
-                    }
-                ]
-            );
+            return $http.get(getAutocompleteUrl(term)).then(function(response) {
+                return response.data.slice(0, 10);
+            });
         }
     });
