712f7aca83eb5ed51ee041903ff9aa43d1b68bc6
Craig <cmcnamara87@gmail.com>
2015-02-03 14:15:32 +1000

    chore(build): clear git repo in dis

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,7 +154,7 @@ module.exports = function (grunt) {
                     src: [
                         '.tmp',
                         '<%= yeoman.dist %>/{,*/}*',
-                        '!<%= yeoman.dist %>/.git*'
+                        '<%= yeoman.dist %>/.git'
                     ]
                 }]
             },
