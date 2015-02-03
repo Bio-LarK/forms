2fab235244a0dec226cd7d8d675cf03e6ea4fa98
Craig <cmcnamara87@gmail.com>
2015-02-03 11:04:14 +1000

    refactor(disorder-service): add autocompletion from server

--- app/scripts/services/disorderservice.js
+++ app/scripts/services/disorderservice.js
@@ -8,7 +8,7 @@
  * Factory in the formsApp.
  */
 angular.module('formsApp')
-    .factory('disorderService', function ($q) {
+    .factory('disorderService', function ($http, $q) {
         return {
             autocomplete: autocomplete
         };
@@ -19,29 +19,41 @@ angular.module('formsApp')
             return getMatchingDisorders(disorder);
         }
 
-        function getMatchingDisorders(disorder) {
-            //http://archive-demo.skeletome.org/drupal/api/disorder.json?parameters%5Bname%5D=achondroplasia
-            return $q.when([
-                {
-                    id: '149',
-                    name: 'Achondroplasia',
-                    uri: 'http://purl.org/skeletome/bonedysplasia#Achondroplasia'
-                },
-                {
-                    id: '402',
-                    name: 'Achondrogenesis type 1A',
-                    uri: 'http://purl.org/skeletome/bonedysplasia#Achondrogenesis_type_1A'
-                },
-                {
-                    id: '453',
-                    name: 'Achondrogenesis type 1B',
-                    uri: 'http://purl.org/skeletome/bonedysplasia#Achondrogenesis_type_1B'
-                },
-                {
-                    id: '477',
-                    name: 'Achondrogenesis type 2',
-                    uri: 'http://purl.org/skeletome/bonedysplasia#Achondrogenesis_type_2'
-                }
-            ]);
+        function getAutocompleteUrl(disorder) {
+            return 'http://archive-demo.skeletome.org/drupal/api/disorder.json?parameters%5Bname%5D=' + disorder;
         }
+
+        function getMatchingDisorders(disorder) {
+            if(!disorder || !disorder.length) {
+                return $q.when([]);
+            }
+            return $http.get(getAutocompleteUrl(disorder)).then(function(response) {
+                return response.data.slice(0, 10);
+            }, function() {
+                // the autocomplete isn't working, use this default
+                return $q.when([
+                    {
+                        id: '149',
+                        name: 'Achondroplasia',
+                        uri: 'http://purl.org/skeletome/bonedysplasia#Achondroplasia'
+                    },
+                    {
+                        id: '402',
+                        name: 'Achondrogenesis type 1A',
+                        uri: 'http://purl.org/skeletome/bonedysplasia#Achondrogenesis_type_1A'
+                    },
+                    {
+                        id: '453',
+                        name: 'Achondrogenesis type 1B',
+                        uri: 'http://purl.org/skeletome/bonedysplasia#Achondrogenesis_type_1B'
+                    },
+                    {
+                        id: '477',
+                        name: 'Achondrogenesis type 2',
+                        uri: 'http://purl.org/skeletome/bonedysplasia#Achondrogenesis_type_2'
+                    }
+                ]);
+            });
+        }
+
     });
